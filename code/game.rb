d36a4474abd713fec07949edccf2171125182afb
Kelly Strickland <imloved@embarqmail.com>
2015-01-12 09:42:42 -0500

    Add suits

--- code/game.rb
+++ code/game.rb
@@ -1,7 +1,7 @@
 # this is going to be a game of blackjack
 # we'll run this within irb, by dealing cards
    # there are 52 cards = 
-      # four suits 
+      # XXfour suits 
         # 1-9 face value;  
         # 10, j, q, k = value 10; 
         # A = value 11
@@ -17,16 +17,24 @@
  
 
 class Game
-  attr_accessor :deck #this means we can
- #interact with the instance variable @grid(@deck)
-  #and we can pretend it's a method: Game.new.grid(deck)
 
+  attr_accessor :deck 
+ 
   def initialize
-    @deck = :the_cards
+    @deck = Deck.new
   end
-
-  
 
 end
 
-  
+class Deck
+
+  attr_accessor :card
+
+  def initialize
+    deck = Array.new(4).times do
+    @card = flat_array.each_slice(4).to_a
+    names = ['2..10' 'J' 'Q' 'K' 'A' ]
+  end
+
+
+end 
--- code/game_test.rb
+++ code/game_test.rb
@@ -4,14 +4,28 @@ require './game'
 
 class TestGame < MiniTest::Unit::TestCase
 
+  def setup
+    @game = Game.new
+  end
+
   def test_game_exists
-    assert Game.new
+    assert @game
   end
 
-  def test_game_deck_exists
-    assert Game.new.deck
+  def test_deck_exists
+    assert @game.deck
   end
-
-
 end
 
+class TestDeck < MiniTest::Unit::TestCase
+
+  def setup
+    @deck = Game.new.deck
+  end
+ 
+  def test_there_are_fiftytwo_cards
+    assert_equal 52, @deck.card.flatten.length
+    assert_equal 4, @deck.names.length
+  end
+
+end
