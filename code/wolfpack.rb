9818c73596717737ab4168f930222d65fc801ef7
Kelly Strickland <imloved@embarqmail.com>
2015-01-12 15:59:15 -0500

    test one

---
+++ code/wolfpack.rb
@@ -0,0 +1,26 @@
+=begin
+A wolfpack is a group of wolves. A wolf has a name and an age. The leader of the wolfpack is the oldest wolf. All wolves are submissive to older wolves and dominant over younger wolves.
+
+A wolf can join a pack. If a wolf joins a pack and becomes leader, the pack throws out the former leader.
+
+A wolf can howl. It howls louder if it's older than 3 than if it's younger. If you call out the name of a wolf to its pack, that wolf will howl.
+
+A wolfpack can take down different sized things depending on how big it is.
+
+A pack of 2 wolves can hunt a gazelle.
+A pack of 5 wolves can get a water buffalo.
+A pack of 30 wolves can eat a rhinoceros.
+=end 
+
+class Wolfpack
+
+	attr_accessor :name, :age
+
+	def initialize(name, age)
+		@name = name
+		@age = age
+	end
+
+	
+
+end
