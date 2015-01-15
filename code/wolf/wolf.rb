b2ec09b9352f0e0c8392ce9eeb33dc89d5cf5004
Kelly Strickland <imloved@embarqmail.com>
2015-01-14 22:13:46 -0500

    Initial commit

---
+++ code/wolf/wolf.rb
@@ -0,0 +1,30 @@
+
+class Wolf
+
+	attr_accessor :name, :age
+
+	def initialize(name, age)
+		@name = name
+		@age = age
+	end
+
+	def howl
+		"Howwwwwl!"
+	end
+
+	def loud_howl
+		@age >= 3	
+	end
+
+	def attack?
+		true
+	end
+
+	def dominant?(young_wolf)
+		@age > young_wolf.age
+	end
+
+	def stays_in_charge?(wolf)
+		@age > wolf.age
+	end
+end
