e5c2ee53b62036cb781b45c555fe502428676a09
Kelly Strickland <imloved@embarqmail.com>
2015-01-19 17:44:21 -0500

    Establish player and player_test. Player had hand, hold cards.

--- code/player.rb
+++ code/player.rb
@@ -1,9 +1,45 @@
 class Player
 
-	attr_reader :hand
+	attr_accessor :hand 
 
-	def initialize(cards)
+	def initialize(cards = [])
 		@hand = cards
+		
+	end
+
+	def take(card)
+		@hand << card
+	end
+
+	def display
+		"Player has #{card_display}"
+	end
+
+	def card_display
+		@hand.collect{|card| card.display}.join(', ')
+	end
+
+	def high_card
+		@hand.max_by{|value| value.size}
+	end
+
+	def pair
+
+	end
+
+	def flush
+
+	end
+
+	def straight
+
+	end
+
+	def three_of_a_kind
+
+	end
+
+	def straight_flush
 
 	end
 
--- code/player_test.rb
+++ code/player_test.rb
@@ -1,18 +1,46 @@
 require 'minitest/autorun'
-require './card'
 require './player'
+require './deck'
 
 
 class PlayerTest < MiniTest::Unit::TestCase
 
-
 	def setup
-		cards = [Card.new(7, :spades), Card.new(2, :hearts), Card.new(13, :diamonds)]
+		cards = [Card.new(7, :spades), Card.new(2, :hearts), Card.new("Ace", :diamonds)]
 		@player = Player.new(cards)
+		@dealer = Player.new(cards)
 	end
 
 	def test_player_has_hand
-		assert @player.hand 
+		assert @player.hand
+		#p @player.inspect
 	end
 
+	def test_dealer_has_hand
+		assert @dealer.hand
+		#p @dealer.inspect
+	end
+ 
+	def test_player_hand_holds_cards
+		card = Card.new(3, :hearts)
+		@player.take card
+		assert @player.hand.include? card
+		p @player.inspect
+	end
+ 
+	def test_player_displays_hand
+		assert_equal "Player has 7 of Spades, 2 of Hearts, Ace of Diamonds", @player.display 
+	end
+
+	#def test_high_card
+	#	 assert_equal 13, @player.high_card.value
+	#end
+
+	def test_pair
+
+	end
+
+	def test_flush
+
+	end
 end
