888fdc98642f356b134e36e15d540d807cd4dbd2
Kelly Strickland <imloved@embarqmail.com>
2015-01-19 20:00:52 -0500

    Added matching cards for pair.

--- code/player.rb
+++ code/player.rb
@@ -4,27 +4,23 @@ class Player
 
 	def initialize(cards = [])
 		@hand = cards
-		
-	end
-
-	def take(card)
-		@hand << card
-	end
-
-	def display
-		"Player has #{card_display}"
-	end
-
-	def card_display
-		@hand.collect{|card| card.display}.join(', ')
 	end
 
 	def high_card
-		@hand.max_by{|value| value.size}
+		@hand.max_by{|card| card.value}
 	end
 
-	def pair
-
+	def has_2_matching_cards?
+		counts = {}
+		@hand.each do |card|
+			puts "counts is #{counts}"
+      puts "value is #{card.value}"
+      puts "counts for that value is #{counts[card.value]}"
+      puts "changing the above to an integer and adding 1 gets #{counts[card.value].to_i + 1}"
+      counts[card.value] = counts[card.value].to_i + 1
+      puts "counts is #{counts}"
+    end
+    counts.any?{|value, number| number == 2 }
 	end
 
 	def flush
--- code/player_test.rb
+++ code/player_test.rb
@@ -8,7 +8,10 @@ class PlayerTest < MiniTest::Unit::TestCase
 	def setup
 		cards = [Card.new(7, :spades), Card.new(2, :hearts), Card.new("Ace", :diamonds)]
 		@player = Player.new(cards)
-		@dealer = Player.new(cards)
+
+		card1 = Card.new(6, :hearts)
+		card2 = Card.new(6, :diamonds)
+		@newhand = Player.new([card1, card2])
 	end
 
 	def test_player_has_hand
@@ -16,31 +19,12 @@ class PlayerTest < MiniTest::Unit::TestCase
 		#p @player.inspect
 	end
 
-	def test_dealer_has_hand
-		assert @dealer.hand
-		#p @dealer.inspect
-	end
- 
-	def test_player_hand_holds_cards
-		card = Card.new(3, :hearts)
-		@player.take card
-		assert @player.hand.include? card
-		p @player.inspect
-	end
- 
-	def test_player_displays_hand
-		assert_equal "Player has 7 of Spades, 2 of Hearts, Ace of Diamonds", @player.display 
+	def test_high_card_value_returned
+		 assert_equal 14, @player.high_card.value
 	end
 
-	#def test_high_card
-	#	 assert_equal 13, @player.high_card.value
-	#end
-
-	def test_pair
-
+	def test_hand_can_have_2_mathching_cards_for_a_pair
+		assert @newhand.has_2_matching_cards?
 	end
 
-	def test_flush
-
-	end
 end
